3194017fafdbfcbe5114f49b97a69ff97b1bcbf0
mona.akhani <mona.akhani2019@gmail.com>
2021-07-01 22:52:54 +0430

    add direction rtl

---
+++ ecommerce-project/src/redux/store.js
@@ -0,0 +1,35 @@
+import { applyMiddleware, createStore } from "redux";
+
+import ReduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import { reducers } from "./reducers/index";
+
+const middlewareEnhancer = applyMiddleware(logger, ReduxThunk);
+const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+
+const store = createStore(reducers, undefined, composedEnhancers);
+
+export default store;
+// const store = createStore(
+//   reducers,
+//   undefined,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
+// const loggerMiddleware = store => next => action => {
+//   console.log("STATE BEFORE", store.getState());
+//   console.log("ACTION DISPATCHED", action);
+//   next(action);
+//   console.log("STATE AFTER", store.getState());
+// };
+
+// const store = createStore(todosReducer, applyMiddleware(loggerMiddleware));
+// const store = createStore(
+//   reducers,
+//   {},
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// export default store;
