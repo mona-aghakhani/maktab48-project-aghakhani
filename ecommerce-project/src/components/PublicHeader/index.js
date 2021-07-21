e637f6a5730c4a502c4cd18630348b314f505d72
mona.akhani <mona.akhani2019@gmail.com>
2021-07-21 10:24:52 +0430

    refactoring publicHeader

--- ecommerce-project/src/components/PublicHeader/index.js
+++ ecommerce-project/src/components/PublicHeader/index.js
@@ -1,47 +1,28 @@
-import {
- AppBar,
-//   Toolbar,
-   Typography,
-    // Box,
-     InputBase,
-    Grid
-} from '@material-ui/core'
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { AppBar, Typography, Grid, Badge, IconButton } from '@material-ui/core'
 // import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from '@material-ui/core/styles';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
-import Badge from '@material-ui/core/Badge';
-import IconButton from '@material-ui/core/IconButton';
+// import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
 import { useStyles } from "./styles"
 
 const StyledBadge = withStyles((theme) => ({
     badge: {
-    //   right: -3,
-    //   top: 13,
-    left:"5px",
-    //   border: `2px solid ${theme.palette.background.paper}`,
-    //   padding: '0 4px',
-      color:"white",
-      background:'rgb(82, 195, 89)'
+        left: "5px",
+        color: "white",
+        background: 'rgb(82, 195, 89)'
     },
-  }))(Badge);
+}))(Badge);
 
 
 const PublicHeader = () => {
-    const { appbar,
-        //  box,
-          link, search, searchIcon, inputRoot, inputInput, grid, item1, item2, item3,badge } = useStyles();
+    const { appbar, link, grid, item1, item2, item3 } = useStyles();
     return (
         <div>
             <AppBar className={appbar} position="static">
-                {/* <Toolbar   className={appbar}> */}
-                {/* <Grid container direction="row">
-                    <Grid item> */}
-                {/* <Box> */}
+
                 <Grid container className={grid} >
                     <Grid item md={3} sm={2} xs={12} className={item1}>
                         <Typography>
@@ -57,73 +38,24 @@ const PublicHeader = () => {
                         spacing={1}
                         className={item2}
                     >
-                        {/* <div className={search}>
-                            <div className={searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase
-                                placeholder="جستجو..."
-                                classes={{
-                                    root: inputRoot,
-                                    input: inputInput,
-                                }}
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                        </div> */}
                     </Grid>
-                    {/* </Box> */}
-                    {/* </Grid> */}
-                    {/* <Grid item alignItems="center">
-                        <Grid item> */}
-                    {/* <Box className={box}> */}
-                    {/* <div> */}
                     <Grid item md={2} sm={4} xs={12} className={item3} container>
-                      {/* <Grid item > */}
                         <NavLink to="/login" exact className={link}  >
-                            <AccountCircleIcon/>
-                           <Typography> مدیریت</Typography>
-                    </NavLink >
-                    {/* </Grid> */}
-                    {/* <div style={{display:"flex"}}> */}
-                    {/* <Grid item > */}
+                            <AccountCircleIcon />
+                            <Typography> مدیریت</Typography>
+                        </NavLink >
                         <NavLink to="/" className={link}>
-                        <IconButton aria-label="cart" color="inherit">
-      <StyledBadge badgeContent={0}
-    //    badgeContent={numItems}
-      >
-        <ShoppingCartIcon />
-      </StyledBadge>
-    </IconButton>
-                        {/* <IconButton aria-label="show 4 new mails" color="inherit">
-                        <Badge badgeContent={4} className={badge}>
-                            <LocalGroceryStoreIcon/>
-                            </Badge>
-            </IconButton>  */}
+                            <IconButton aria-label="cart" color="inherit">
+                                <StyledBadge badgeContent={0}
+                                //    badgeContent={numItems}
+                                >
+                                    <ShoppingCartIcon />
+                                </StyledBadge>
+                            </IconButton>
                             <Typography>سبد خرید</Typography>
-                    </NavLink>
-                    {/* </Grid> */}
-                    {/* </div> */}
-                        {/* <NavLink to="/admin/login" exact >
-      login
-      </NavLink> */}
-                        {/* </div> */}
-                        {/* </Grid>
-                    <Grid item> */}
-                        {/* <div> */}
-
-                        {/* <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton> */}
-                        {/* </div>
-                    </Box> */}
-                        {/* </Grid>
-                    </Grid>
-                    </Grid> */}
+                        </NavLink>
                     </Grid>
                 </Grid>
-                {/* </Toolbar> */}
             </AppBar>
 
         </div>
--- ecommerce-project/src/components/PublicHeader/styles.js
+++ ecommerce-project/src/components/PublicHeader/styles.js
@@ -1,89 +1,38 @@
-import { makeStyles,fade  } from "@material-ui/core";
-// import { COLORS } from "../../theme/constantVariable";
-export const useStyles=makeStyles((theme)=>({
-    appbar:{
-        minHeight:"30px",
-        justifyContent:"space-between",
-        padding:"10px",
-        alignItems:"center"
-    },
-    grid:{
-      alignItems:"center",
-      justifyContent:"center"
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  appbar: {
+    minHeight: "30px",
+    justifyContent: "space-between",
+    padding: "10px",
+    alignItems: "center"
   },
-   item1:{
-    textAlign:"center"
-},
-item2:{
-  justifyContent:"center",
-  margin:"10px 0"
-  
-},
-item3:{
-  textAlign:"center",
-  textDecoration:"none",
-  alignItems:"center",
-  justifyContent:"center",
-},
-    box:{
-        display:"flex",
-        justifyContent:"space-between"
-    },
-    link:{
-        // marginRight:"20px",
-        // textDecoration:"none",
-        textDecoration:"none",
-        color:"black",
-        fontSize:"13px",
-        display:"flex",
-        alignItems:"center",
-        // margin:"10px 0 0 5px",
-        // width:"60px",
-        // color: COLORS.textColor,
-        '&:hover':{
-          textDecoration:"none",
-          borderBottom:"1px solid rgb(214,230,215)",
-      }
-    },
-    // badge:{
-    //   color:"white",
-    //   background:"green"
-    // }
-    // search: {
-    //     position: 'relative',
-    //     borderRadius: theme.shape.borderRadius,
-    //     backgroundColor: fade(theme.palette.common.white, 0.15),
-    //     '&:hover': {
-    //       backgroundColor: fade(theme.palette.common.white, 0.25),
-    //     },
-    //     marginRight: theme.spacing(2),
-    //     marginLeft: 0,
-    //     width: '100%',
-    //     [theme.breakpoints.up('sm')]: {
-    //       marginLeft: theme.spacing(3),
-    //       width: 'auto',
-    //     },
-    //   },
-    //   searchIcon: {
-    //     padding: theme.spacing(0, 2),
-    //     height: '100%',
-    //     position: 'absolute',
-    //     pointerEvents: 'none',
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    //   },
-    //   inputRoot: {
-    //     color: 'inherit',
-    //   },
-    //   inputInput: {
-    //     padding: theme.spacing(1, 1, 1, 0),
-    //     // vertical padding + font size from searchIcon
-    //     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    //     transition: theme.transitions.create('width'),
-    //     width: '100%',
-    //     [theme.breakpoints.up('md')]: {
-    //       width: '20ch',
-    //     },
-    //   },
+  grid: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  item1: {
+    textAlign: "center"
+  },
+  item2: {
+    justifyContent: "center",
+    margin: "10px 0"
+  },
+  item3: {
+    textAlign: "center",
+    textDecoration: "none",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  link: {
+    textDecoration: "none",
+    color: "black",
+    fontSize: "13px",
+    display: "flex",
+    alignItems: "center",
+    '&:hover': {
+      textDecoration: "none",
+      borderBottom: "1px solid rgb(214,230,215)",
+    }
+  },
 }))
